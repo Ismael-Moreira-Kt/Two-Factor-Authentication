9c650670c01dcbf1694e5a29a873dc36407ad9f5
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-03-13 15:48:01 +0000

    feat: Finished how to generate the master key

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import static org.example.master_key.MasterKey.generateMasterKey;
+
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        String masterKey = generateMasterKey();
+        
+    }
 }
