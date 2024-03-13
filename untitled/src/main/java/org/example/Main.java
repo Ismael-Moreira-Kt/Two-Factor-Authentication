75974d7519d81e0977d4e88c11d2f6fa1ef3312a
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-03-13 16:03:30 +0000

    feat: Created the way to generate the final code.

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,12 +1,22 @@
 package org.example;
 
+import com.eatthepath.otp.TimeBasedOneTimePasswordGenerator;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.GeneralSecurityException;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import static org.example.master_key.MasterKey.generateMasterKey;
 
 
 public class Main {
-    public static void main(String[] args) {
-        String masterKey = generateMasterKey();
-        
+    private static final byte[] MASTER_KEY_BYTES = generateMasterKey();
+    private static final SecretKeySpec MASTER_KEY = new SecretKeySpec(MASTER_KEY_BYTES, "AES");
+
+    public static void main(String[] args) throws GeneralSecurityException {
+        TimeBasedOneTimePasswordGenerator totp = new TimeBasedOneTimePasswordGenerator();
+        Instant now = Instant.now();
+
+        int code = totp.generateOneTimePassword(MASTER_KEY, now);
     }
 }
--- untitled/src/main/java/org/example/master_key/MasterKey.java
+++ untitled/src/main/java/org/example/master_key/MasterKey.java
@@ -1,15 +1,14 @@
 package org.example.master_key;
 
 import java.security.SecureRandom;
-import java.util.Base64;
 
 public class MasterKey {
-    public static String generateMasterKey() {
+    public static byte[] generateMasterKey() {
         SecureRandom random = new SecureRandom();
 
         byte[] bytes = new byte[32];
         random.nextBytes(bytes);
 
-        return Base64.getEncoder().encodeToString(bytes);
+        return bytes;
     }
 }
